3808dcd109636f7a285709ba3909422d301efc66
Rahul Patil <loginrahul90@gmail.com>
2013-10-09 22:35:07 +0530

    Update ldapPasswdNotify.py

--- ldapPasswdNotify.py
+++ ldapPasswdNotify.py
@@ -1,85 +1,84 @@
 #!/usr/bin/env python
 # Author :- Rahul Patil<linuxian.com>
-# Date :- Thu Sep 26 23:46:09 IST 2013
+
 
 import sys
-import os
+import ldap 
+import datetime
+import subprocess
 
 #-----------------------------------------------------------------
 # Provide Ldap DN Details to Perform Ldap Search using anonymouse
 #-----------------------------------------------------------------
-Domain = 'linuxian.com'
-EmailDomain = 'abc.om' # if your mail domain is different this will becom user@abc.com 
-ConnectDC = 'ldap://localhost:389'
-# if 14 days remains to expire password then it will send email to that user 
-# until user update the new password
-PwdWarnDays = 14  
-pwdMaxAge = 45 # default password expire in 45 days as per ldap ppolicy
+domain_name = 'linuxian.com'
+email_domain = 'linuxian.com' # if your mail domain is different this will becom user@abc.com 
+connect_dc = 'ldap://localhost:389'
 
-Subject = "Ldap Password Expiry Details"
+# Define number of days to send warning alert 
+# suppose you have define 14 days and pwd_max_age age is 45 days 
+# then it will notify users when 14 days remain to expire password 
+# it will send email to that user until user update the new password
+pwd_warn_days = 14 
+# default password expire in  day as per ldap ppolicy
+pwd_max_age = 45 # password should change in days
+mail_subject = "Ldap Password Expiry Details"
 
-MsgBody = """
+mail_body = """
 Dear %s,
-	Your Password Will be Expire in %s, we request you to please
-change your password, your last password change date is %s
+     Your password will expire in %s day(s). We're sorry for the
+inconvenience, but we need you to change your password, your 
+last password date is %s.
 
 Best Regards,
 Linux Admin
 """
 
 
-def GetUserDetails():
-    	""" This Function Will save all details in file
-    	it will use ldap search query for the same."""
+def get_user_details():
+    	""" Return UID and Last Password Details """
     	# Create bind dn eg. dc=example,dc=com
-    	BindDN = ','.join([ 'dc=' + d for d in Domain.split('.') ])
-    	#
-    	import ldap 
-    	l = ldap.initialize(ConnectDC)
+    	bind_dn = ','.join([ 'dc=' + d for d in domain_name.split('.') ])
+    	l = ldap.initialize(connect_dc)
     	# Perform Ldap Search
     	return  l.search_s(
-    			BindDN,
+    			bind_dn,
     			ldap.SCOPE_SUBTREE,
     			'(uid=*)',['uid','pwdChangedTime']
     		)
 
 
-def CheckExpiry():
-        """ 
-        This Function will Check each user ExpiryWarning
+def check_password_expiry():
+	"""Check each user ExpiryWarning
         if it more thans WarningDays then it will send Emails
-        to that particuler user
-        """
-      	 import datetime
-         for k,v in Users:
-              		uid = ''.join(v['uid'])
-              		if 'pwdChangedTime' not in v:
-              				pass
-                		  	#print "User " + uid + "  not Updated Password" 
-              		try:
-              	  		  l = ''.join(v['pwdChangedTime'])
-              		except:
-              		    	pass
-              		
-              		if 'pwdChangedTime' in v:
-              			# To extrace year month day
-              			d1 = datetime.date.today()
-              			d2 = datetime.date(int(l[0:4]),int(l[4:6]),int(l[6:8]))
-              			DaysOfPasswordChange = (d1 - d2).days
-              			d2 = d2.strftime('%d, %b %Y')
-              
-              			ExpireIn = pwdMaxAge - DaysOfPasswordChange
-              
-              			# if password not changed before 14 days 
-              			if ExpireIn <= PwdWarnDays:
-              				SendMail = "echo '" + MsgBody % (uid,ExpireIn,d2) + "' \
-              						  mail -s " + '"' + Subject + '"' + ' ' + \
-              						  uid + '@' + EmailDomain 
-              				#os.system(SendMail)
-              				print SendMail
+        to that particuler user"""
+      	for k,v in ldap_users:
+		uid = ''.join(v['uid'])
+             	if 'pwdChangedTime' not in v:
+			# means password not changed yet from user creation date
+			pwd_expire_in_days = 0 
+             	  	#print "User " + uid + "  not Updated Password" 
+             	try:
+               	  	l = ''.join(v['pwdChangedTime'])
+             	except:
+             	    	pass
+             	
+             	if 'pwdChangedTime' in v:
+             		# Date Calculation to get number of days remains to change password
+			d1 = datetime.date.today()
+             		d2 = datetime.date(int(l[0:4]),int(l[4:6]),int(l[6:8]))
+			# to get number of days password has updated 
+             		days_of_password_change = (d1 - d2).days
+             		d2 = d2.strftime('%d, %b %Y')
+               		pwd_expire_in_days = pwd_max_age - days_of_password_change
+                
+             	if pwd_expire_in_days <= pwd_warn_days:
+			p = subprocess.Popen(['mail', '-s', mail_subject, uid + '@' + email_domain],
+						stdin=subprocess.PIPE)
+			p.communicate(mail_body % (uid,pwd_expire_in_days,d2))
 
 
 
 if __name__ == '__main__':
-        Users = GetUserDetails()
-        CheckExpiry()
+        ldap_users = get_user_details()
+        check_password_expiry()
+
